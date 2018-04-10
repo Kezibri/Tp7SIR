67748ddece571480e3d4040a7c27ae854053dbfc
Anas Kezibri <anas.kezibri@gmail.com>
2018-04-10 22:47:15 +0200

    Person services implementation

--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Person} from './person.model';
 
 @Injectable()
 export class PersonService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public getPeople() {
+    return this.http.get<Person[]>('rest/person');
+  }
+
+  public getPersonById(id: number) {
+    return this.http.get<Person>('rest/person/' + id);
+  }
+
+  public createPerson(person) {
+    const _http = new HttpHeaders();
+    const hhttp = _http.append('Content-Type', 'application/json');
+    const options =  {
+      http: http
+    };
+    return this.http.post<Person>('rest/person', person, options);
+  }
 
 }
