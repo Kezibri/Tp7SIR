82170e30a95d37541726002996d9664fe0e791ed
Anas Kezibri <anas.kezibri@gmail.com>
2018-04-10 22:51:35 +0200

    CONFIG : app Routing

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,21 @@
+ import { NgModule } from '@angular/core';
+    import { RouterModule, Routes } from '@angular/router';
+    import { PersonComponent } from '../person/person.component'
+
+    const routes: Routes = [
+        {
+            path: 'people',
+            component: PersonComponent,
+        },
+    ];
+
+    @NgModule({
+        imports: [
+            RouterModule.forRoot(routes)
+        ],
+        exports: [
+            RouterModule
+        ],
+        declarations: []
+    })
+    export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
 import { PersonService } from './person.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
 @NgModule({
@@ -13,7 +16,10 @@ import { PersonService } from './person.service';
     PersonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	AppRoutingModule,
+	FormsModule,
+    HttpClientModule
   ],
   providers: [
   PersonService
